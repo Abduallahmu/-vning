b1939b63ed73aa8447ef027ddfbc993d8ad893b2
deltagare <deltagare@LEXJONKAF8>
2019-01-28 10:08:36 +0100

    Add project files.

---
+++ övning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
